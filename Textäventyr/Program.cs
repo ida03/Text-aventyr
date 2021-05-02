cc56a1c811599bd6943720e9ff21fa46015a12aa
ida03 <73817507+ida03@users.noreply.github.com>
2021-05-02 16:01:02 +0200

    Add project files.

---
+++ Textäventyr/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Textäventyr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Något känns konstigt. Din mjuka madrass är utbytt mot något hårt som skaver in i din rygg.");
+            Console.WriteLine("Ljudet av droppande vatten hörs runt dig.");
+            Console.WriteLine("När du öppnar dina ögon ser du att du inte är i din säng hemma, utan i vad som ser ut som en mörk fängelsecell.");
+
+            Console.WriteLine(" - Ah, jag ser att du äntligen har vaknat.");
+
+            Console.WriteLine("Rösten kommer från din högra sida och där, i cellen bredvid, sitter en mycket gammal man");
+            Console.WriteLine(" -Jag hoppades att du skulle vakna snart. När vakterna bar in dig var du medvetslös och jag visste inte hur mycket sömnmedel de hade gett dig.");
+            Console.WriteLine("Du undrar säkert var du är och det ska jag berätta för dig, men först vill jag veta ditt namn.");
+        }
+    }
+}
