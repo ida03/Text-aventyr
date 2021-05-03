e1443731dc6881a489dcfa6578e006581e6d2f51
ida03 <73817507+ida03@users.noreply.github.com>
2021-05-03 10:41:07 +0200

    Lagt till mer story och kommentarer för saker som ska göras

--- Textäventyr/Program.cs
+++ Textäventyr/Program.cs
@@ -10,15 +10,31 @@ namespace Textäventyr
     {
         static void Main(string[] args)
         {
+            int liv = 3;
             Console.WriteLine("Något känns konstigt. Din mjuka madrass är utbytt mot något hårt som skaver in i din rygg.");
             Console.WriteLine("Ljudet av droppande vatten hörs runt dig.");
             Console.WriteLine("När du öppnar dina ögon ser du att du inte är i din säng hemma, utan i vad som ser ut som en mörk fängelsecell.");
-
             Console.WriteLine(" - Ah, jag ser att du äntligen har vaknat.");
-
             Console.WriteLine("Rösten kommer från din högra sida och där, i cellen bredvid, sitter en mycket gammal man");
             Console.WriteLine(" -Jag hoppades att du skulle vakna snart. När vakterna bar in dig var du medvetslös och jag visste inte hur mycket sömnmedel de hade gett dig.");
             Console.WriteLine("Du undrar säkert var du är och det ska jag berätta för dig, men först vill jag veta ditt namn.");
+            //sätt in namn här
+            Console.WriteLine("Den gamla mannen räcker över en sliten och fuktig pappersbit. På den står det:");
+            Console.WriteLine("Här är koden till det nya låset, låt inte fångarna få tag på den");
+            Console.WriteLine("1. Roten ur 4, multiplicerat med 6");
+            Console.WriteLine("2. 4 plus 4, dividerat med 2");
+            Console.WriteLine("3. 5 multiplicerat med 0");
+            Console.WriteLine("Du kryper fram mot låset som hänger på andra sidan om din celldörr");
+            //sätt in lösning för låskod här
+
+
+            Console.WriteLine("Låset slår i stengolvet och dörren går upp. Du sticker ut huvudet för att se så att ingen av vakterna hörde");
+            Console.WriteLine(" -Bra gjort! Gå nu innan någon kommer för att kolla till dig.");
+            Console.WriteLine("Tänk inte på mig, jag är bara en gammal man, om de hade velat skada mig hade de gjort det för länge sedan");
+            Console.WriteLine("Du går ut i korridoren mellan cellerna. Längst bort syns en dörr, du skyndar mot den");
+            Console.WriteLine("Dörren glider upp på knarrande gångjärn och du hoppas innerligt att ingen hörde. Korridoren utanför är tyst och övergiven, det enda som hörs är dina fotsteg");
+            Console.WriteLine("Plötsligt hör du fotsteg som kommer mot dig. Innan du hinner gömma dig rundar en man hörnet och får syn på dig.");
+
         }
     }
 }
